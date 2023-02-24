38fcb58dbacec541b8182a554e5b27156060d59e
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2023-02-25 01:39:43 +0800

    refactor: table and schema referencing

--- macros/drop_tables.sql
+++ macros/drop_tables.sql
@@ -1,7 +1,7 @@
 {% macro drop_tables(table_names) %}
     {% for table_name in table_names %}
         {% set drop_query %}
-          drop table if exists imdb_test.{{ table_name }};
+          drop table if exists {{ source('denormalize', table_name) }};
         {% endset %}
         {% do run_query(drop_query) %}
     {% endfor %}

81cbf4a4b9cc9b19f964cc23f972019835e01de9
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2023-02-25 02:55:20 +0800

    add: foreign key relationships

---
+++ denormalize/table_relations.sql
@@ -0,0 +1,12 @@
+{{
+  config(
+    materialized='ephemeral'
+  )
+}}
+
+{{ add_foreign_key_constraint('movies', 'id', 'movie_directors', 'movie_id') }}
+
+{{ add_foreign_key_constraint('movies', 'id', 'role_actors', 'movie_id') }}
+
+{{ add_foreign_key_constraint('movies', 'id', 'movies_genres', 'movie_id') }}
+
---
+++ macros/add_relations.sql
@@ -0,0 +1,11 @@
+{% macro add_foreign_key_constraint(parent_table, parent_column, child_table, child_column) %}
+  {% set relation_query %}
+
+    ALTER TABLE {{ source('denormalize', child_table) }} 
+    ADD CONSTRAINT fk_{{ parent_table }}_{{ parent_column }}_{{  child_table }}_{{ child_column }}
+    FOREIGN KEY ({{ child_column }})
+    REFERENCES {{ source('denormalize', parent_table) }}({{ parent_column }});
+
+  {% endset %}
+  {% do run_query(relation_query) %}
+{% endmacro %}
